a2787a60559548b085edbfd372d0e85fd88255e3
Alfredo Sánchez <alfresanc@ciencias.unam.mx>
2017-01-26 15:08:26 -0600

    included 'encuestas.apps.EncuestasConfig'

--- wsgi/localbosc/localbosc/settings.py
+++ wsgi/localbosc/localbosc/settings.py
@@ -44,6 +44,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'encuestas.apps.EncuestasConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
